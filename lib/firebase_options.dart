272a64186c381d5044541fa06ac3968f0c706b84
mike <miguelcontrerassousa@gmail.com>
2025-05-09 08:54:11 +0200

    difrentes mejoras y rendimiento

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -58,5 +55,15 @@ class DefaultFirebaseOptions {
     messagingSenderId: '672901646033',
     projectId: 'vecigest-e164e',
     storageBucket: 'vecigest-e164e.firebasestorage.app',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBCwPizahc6PZvFoADfvlJK0VJ5ggXT108',
+    authDomain: 'vecigest-e164e.firebaseapp.com',
+    projectId: 'vecigest-e164e',
+    storageBucket: 'vecigest-e164e.firebasestorage.app',
+    messagingSenderId: '672901646033',
+    appId: '1:672901646033:web:e3c03f5a21619defa927f0',
+    measurementId: 'G-WM4P5WLBYN',
   );
 }
